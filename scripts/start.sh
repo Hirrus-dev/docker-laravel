b2bcb1f88f3b65fb078a33bd3f116ccaf398756b
Hirrus-dev <68560773+Hirrus-dev@users.noreply.github.com>
2020-12-08 17:31:41 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -231,4 +231,5 @@ EOF
     sudo docker exec -it php bash -c "cp /.env /var/www/$domainname/laravel/.env"
     sudo docker exec -it php bash -c "composer update --no-scripts -d /var/www/$domainname/laravel/"
     sudo docker exec -it php bash -c "php artisan migrate"
+    sudo crontab -l | { cat; echo "*/1 * * * * docker exec php bash -c \"cd /vaw/www/$domainname && sudo -u www-data artisan shedule:run\""; } | sudo crontab -
 fi
